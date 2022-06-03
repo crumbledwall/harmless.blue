6ed6e919e9fd5714a77665cbd7cd2efd93f78585
crumbledwall <mail@harmless.blue>
2022-06-03 16:01:20 +0800

    feat: add highlight add remove default width

--- pages/posts/[id].tsx
+++ pages/posts/[id].tsx
@@ -1,15 +1,18 @@
-import Layout from '@/components/layout'
+import Layout from '@/components/common/layout'
 import { getPost } from '@/lib/yuque'
 import { parseMarkdown } from '@/lib/paser'
 import { timeFormat } from '@/lib/time'
+import 'highlight.js/styles/github-dark.css'
 
 export default function Post({ postData }) {
   return (
     <Layout pageName={postData?.title}>
-      <div className="flex flex-col items-center container py-10">
-        <div className="text-3xl font-bold my-20">{postData?.title}</div>
-        <div>{timeFormat(postData?.time)}</div>
-        <article className="prose">{postData ? parseMarkdown(postData.content) : ''}</article>
+      <div className="flex flex-col items-center container py-5">
+        <div className="text-4xl font-bold mt-5">{postData?.title}</div>
+        <div className="my-4">{timeFormat(postData?.time)}</div>
+        <article className="prose max-w-none">
+          {postData ? parseMarkdown(postData.content) : ''}
+        </article>
       </div>
     </Layout>
   )
