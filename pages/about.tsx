8792bb4f82107914d83ed33d9387bccfdc2824f0
crumbledwall <mail@harmless.blue>
2022-06-03 22:43:27 +0800

    feat: update page style

--- pages/about.tsx
+++ pages/about.tsx
@@ -5,9 +5,11 @@ import { parseMarkdown } from '@/lib/paser'
 export default function Post({ postData }) {
   return (
     <Layout pageName={postData?.title}>
-      <div className="flex flex-col items-center container py-10">
-        <div className="text-3xl font-bold my-20">{postData?.title}</div>
-        <article className="prose">{postData ? parseMarkdown(postData.content) : ''}</article>
+      <div className="flex flex-col items-start container py-5">
+        <div className="text-4xl font-bold mt-5 font-serif">{postData?.title}</div>
+        <article className="prose max-w-none">
+          {postData ? parseMarkdown(postData.content) : ''}
+        </article>
       </div>
     </Layout>
   )
